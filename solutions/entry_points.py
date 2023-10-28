3f0b7851f9f0b92662dff0331e9b31ca4c5772c1
Tomas Twardzik <twardziktom@gmail.com>
2023-10-29 00:51:40 +0200

    [add] adding entrypoints into pyproject.toml adding entry_points module adding __main__ hook for exercise_two

---
+++ solutions/entry_points.py
@@ -0,0 +1,23 @@
+"""
+Default setuptools entry point does not allow for argument, so one needs to use argparse similar. I prefer 'click'.
+"""
+
+from exercise_two import show_dependency_graph, TARGET_PATH
+from exercise_one import detect_duplicate_elements
+import click
+
+@click.command()
+@click.option("-f", "--file_path",
+              default=TARGET_PATH,
+              show_default = True,
+              type=str,
+              help="Defines absolute path to a JSON file containing dependency relations.")
+
+def dependency_graph(file_path):
+    show_dependency_graph(file_path)
+    
+    
+@click.command()
+@click.argument('elements', nargs=-1)
+def detect_duplicate(elements):
+    print(detect_duplicate_elements(elements))
---
+++ solutions/exercise_two/__main__.py
@@ -0,0 +1,5 @@
+
+from exercise_two import show_dependency_graph
+
+if __name__ == '__main__':
+    show_dependency_graph()
