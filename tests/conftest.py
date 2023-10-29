bf9fab825a53ffd15fafc5323cd2dfa3d3c804bc
Tomas Twardzik <twardziktom@gmail.com>
2023-10-29 22:32:50 +0100

    [add] adding new fixture with example package structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 # system imports
 import sys
 
-
 # third-party imports
 import pytest
+
+# local import
+from exercise_two.exercise_two import Package
 
 
 @pytest.fixture
@@ -17,3 +19,15 @@ def log_stdout(monkeypatch):
 
     monkeypatch.setattr(sys.stdout, 'write', redirected_write)
     return captured_stdout
+
+
+@pytest.fixture
+def example_structure():
+    p1 = Package("pkg1")
+    p1.dependencies = [Package("pkg2", depth_level=1), Package("pkg3", depth_level=1)]
+    p1.dependencies[0].dependencies = [Package("pkg3", depth_level=2)]
+    p2 = Package("pkg2")
+    p2.dependencies = [Package('pkg3', depth_level=1)]
+    p3 = Package("pkg3")
+    package_structure = [p1, p2, p3]
+    return package_structure
