500c5d9cfb49c8a4cad3a02d7e0906c0e83acd95
Tomas Twardzik <twardziktom@gmail.com>
2023-11-04 23:44:20 +0100

    [fix] reworked tasting to facilitate changes in exercise_two supports new dependency_tree type added new tests

--- tests/test_exercise_two.py
+++ tests/test_exercise_two.py
@@ -56,6 +56,12 @@ def test_file_presence():
         assert dr.resolve_graph(add_test_path("non_existent_path.json"))
 
 
+def test_empty_json_load():
+    with pytest.raises(json.JSONDecodeError):
+        dr = DependencyResolver()
+        assert dr.resolve_graph(add_test_path("empty.json"))
+
+
 def test_invalid_json_load():
     with pytest.raises(json.JSONDecodeError):
         dr = DependencyResolver()
@@ -82,21 +88,39 @@ def test_dependency_presence():
 
 @pytest.mark.parametrize('file_name',
                          [('cyclic_import.json'),
-                          ('self_import.json'),])
+                          ('self_import.json'),
+                          ('cyclic_import_worst_case_fail.json')])
 def test_cyclic_import(file_name):
     with pytest.raises(CyclicDependencyError):
         dr = DependencyResolver()
         assert dr.resolve_graph(add_test_path(file_name))
 
 
+def test_cyclic_import_pass():
+    p1 = Package('pkg1')
+    p2 = Package('pkg2')
+    p3 = Package('pkg3')
+    p4 = Package('pkg4')
+    p1.dependencies = [p2]
+    p2.dependencies = [p3]
+    p3.dependencies = [p4]
+    comp_dict = dict(pkg1=p1,
+                     pkg2=p2,
+                     pkg3=p3,
+                     pkg4=p4)
+
+    dr = DependencyResolver()
+    assert dr.resolve_graph(add_test_path(add_test_path('cyclic_import_worst_case_pass.json'))) == comp_dict
+
+
 def test_empty_dependencies():
     dr = DependencyResolver()
-    assert dr.resolve_graph(add_test_path("empty_dependencies.json")) == []
+    assert dr.resolve_graph(add_test_path("empty_dependencies.json")) == {}
 
 
 def test_simple_package():
     dr = DependencyResolver()
-    assert dr.resolve_graph(add_test_path("simple_dependencies.json")) == [Package("pkg1")]
+    assert dr.resolve_graph(add_test_path("simple_dependencies.json")) == {'pkg1': Package("pkg1")}
 
 
 @pytest.mark.parametrize('file_name',
@@ -115,23 +139,24 @@ def test_structured_print(log_stdout):
     dr = DependencyResolver()
     dr.print_dependency_graph(add_test_path("deps.json"))
 
-    comp_string = "- pkg1\n"       \
-                  "  - pkg2\n"    \
-                  "    - pkg3\n" \
-                  "  - pkg3\n"    \
+    comp_string = "- pkg3\n"       \
                   "- pkg2\n"       \
-                  "  - pkg3\n"    \
-                  "- pkg3\n"
+                  "  - pkg3\n"     \
+                  "- pkg1\n"       \
+                  "  - pkg2\n"     \
+                  "    - pkg3\n"   \
+                  "  - pkg3\n"
+
     assert log_stdout["stdout"] == comp_string
 
 
 def test_show_dependency_graph(log_stdout):
     show_dependency_graph(add_test_path("deps.json"))
-    comp_string = "- pkg1\n"       \
-                  "  - pkg2\n"    \
-                  "    - pkg3\n" \
-                  "  - pkg3\n"    \
+    comp_string = "- pkg3\n"       \
                   "- pkg2\n"       \
-                  "  - pkg3\n"    \
-                  "- pkg3\n"
+                  "  - pkg3\n"     \
+                  "- pkg1\n"       \
+                  "  - pkg2\n"     \
+                  "    - pkg3\n"   \
+                  "  - pkg3\n"
     assert log_stdout["stdout"] == comp_string
